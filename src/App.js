a870b465f3beab97b1889f1be37cd136f4d5a1ab
joel2theworld <joel2thewrld@gmail.com>
2024-11-13 08:06:26 +0100

    final v1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react'
-import Home from './pages/Home'
+import React from "react";
+import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import About from './pages/About';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
-import './js/main.js';
-import 'waypoints/lib/jquery.waypoints.min.js'; 
-import Services from './pages/Services.jsx';
-
+import { Helmet } from "react-helmet";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import "./js/main.js";
+import "waypoints/lib/jquery.waypoints.min.js";
+import Services from "./pages/Services.jsx";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path='/about' element={<About/>}/>
-      <Route path='/projects' element={<Projects />}/>
-      <Route path='/contact' element={<Contact />}/>
-      <Route path='/services' element={<Services/>}/>
-    </Routes>
-    </Router>
-  )
-}
+    <div>
+      <Helmet>
+        <title>High Park | Visionary Architecture, Modern Creations.</title>
+        <meta name="description" content="Premier Architects specializes in innovative architectural designs across Nigeria, featuring projects like Lekki Beach Front and Jade's Court in Lagos." />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      </Helmet>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/services" element={<Services />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
-export default App
+export default App;
